1228bbc5ece18fc8df9619413b29843c99c186c7
Alvaro Atencio <alvaroatencio123@gmail.com>
2024-06-24 20:52:25 -0300

    modelo nav

--- nelux-technology/src/organisms/Form/Nav.js
+++ nelux-technology/src/organisms/Form/Nav.js
@@ -5,11 +5,19 @@ import React from 'react'
 const Nav = () => {
     return (
         <div>
-            <ul>
-                <li>Home</li>
-                <li>Carrito</li>
-                <li>Galeria</li>
-                <li>Productos</li>
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Products</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Gallery</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Contact</a>
+                </li>
             </ul>
         </div>
     )
