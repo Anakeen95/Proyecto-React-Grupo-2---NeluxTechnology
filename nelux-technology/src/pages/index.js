3f84a271a83452cdb1101c1a34192ab824b63b74
bravo leticia <bravoleticia08@gmail.com>
2024-06-25 21:01:37 -0300

    Colocación de banner home (sin responsive)

--- nelux-technology/src/pages/index.js
+++ nelux-technology/src/pages/index.js
@@ -1,9 +1,12 @@
 import SectionCard from "../molecules/SectionCards"
+import BannerHome from "../organisms/Banner/bannerHome";
+//import bannerImage3 from "./banner-home.jpg"
 
 
 const index = () => {
   return (
     <main>
+     <BannerHome></BannerHome>
      <SectionCard/>
     </main>
   );
