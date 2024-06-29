a3753893815cb30044b6af095ebe797d05fee8f7
Alvaro Atencio <alvaroatencio123@gmail.com>
2024-06-29 19:54:00 -0300

    Nav y Footer responsive

--- nelux-technology/src/organisms/Nav/Nav.js
+++ nelux-technology/src/organisms/Nav/Nav.js
@@ -1,33 +1,44 @@
 
-import React from 'react';
-import styles from './Nav.module.css'
+import React, { useState } from 'react';
+import styles from './Nav.module.css';
 
 
 const Nav = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <div className={styles.nav}>
-            <div class={styles.logo}>
-                <img src="./images/NeluxTech-Isotipo-Negro.png" alt="Logo" ></img>
+            <div className={styles.logo}>
+                <img src="./images/NeluxTech-Isotipo-Negro.png" alt="Logo" />
             </div>
-            <ul className={styles.navBar}>
+            <div className={`${styles.navBar} ${isOpen ? styles.navBarOpen : ''}`}>
                 <li className={styles.navBarItem}>
-                    <a className="navbar-link" href="Home">Home</a>
+                    <a className={styles.navBarLink} href="#Home">Home</a>
                 </li>
                 <li className={styles.navBarItem}>
-                    <a className="navbar-link" href="Productos">Products</a>
+                    <a className={styles.navBarLink} href="#Productos">Products</a>
                 </li>
                 <li className={styles.navBarItem}>
-                    <a className="navbar-link" href="Galleria">Gallery</a>
+                    <a className={styles.navBarLink} href="#Galleria">Gallery</a>
                 </li>
                 <li className={styles.navBarItem}>
-                    <a className="navbar-link" href="Contacto">Contact</a>
+                    <a className={styles.navBarLink} href="#Contacto">Contact</a>
                 </li>
-            </ul>
-            <div class={styles.cartLogo}>
-                <img src="./images/Cart-Icon.png" alt="Cart-Logo" ></img>
+            </div>
+            <div className={styles.cartLogo}>
+                <img src="./images/Cart-Icon.png" alt="Cart-Logo" />
+            </div>
+            <div className={styles.burgerMenu} onClick={toggleMenu}>
+                <div className={styles.burgerLine}></div>
+                <div className={styles.burgerLine}></div>
+                <div className={styles.burgerLine}></div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Nav
+export default Nav;
