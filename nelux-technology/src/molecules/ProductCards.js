4066768312c5ce238483164187d638ed77f182a3
Alvaro Atencio <alvaroatencio123@gmail.com>
2024-07-15 19:35:07 -0300

    limpieza de codigo

--- nelux-technology/src/molecules/ProductCards.js
+++ nelux-technology/src/molecules/ProductCards.js
@@ -1,152 +1,47 @@
-//  import React, { useState } from 'react';
-//  import Button from "../atoms/Button/Button";
-//  import Modal from './CartCardModal';
-//  import styles from "./Cards.module.css";
-//  import Buttoninfo from '../atoms/Button/buttoninfo';
-//  import {Modal, ModalHeader,ModalBody, ModalFooter} from 'reactstrap';
-//  import { ModalBody } from 'reactstrap';
-//  import ProductsModal from './ProductsModal';
 
-//  const ProductCard = ({ product }) => {
-//    const [showModal, setShowModal] = useState(false);
-//    const [modalType, setModalType] = useState(null);
-  
+import React, { useState } from 'react';
+import Button from "../atoms/Button/Button";
+import CartCardModal from './CartCardModal';
+import styles from "./Cards.module.css";
+import Buttoninfo from '../atoms/Button/buttoninfo';
+import ProductsModal from './ProductsModal';
 
-//  const handleButtonClick = () => {
-//    setShowModal(true);
-//  };
-//     const handleButtonClick = (type) => {
-//       setModalType(type); // Establecer el tipo de modal según el botón clicado
-//       setShowModal(true); // Mostrar el modal
-//     };
-//    const handleCloseModal = () => {
-//      setShowModal(false); // Cerrar el modal
-//      setModalType(null); // Reiniciar el tipo de modal
-//    };
+const ProductCard = ({ product }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [modalType, setModalType] = useState(null);
 
-//  const handleCloseModal = () => {
-//    setShowModal(false);
-//  };
-
-  
-
-
-
-
-//    return (
-//      <>  
-//        <div className={styles.card}>
-//          <img src={product.imageUrl} alt={product.name} className={styles.img}/>
-//          <div>
-//            <h5 className={styles.cardTitle}>{product.name}</h5>
-//            <p className={styles.cardText}>{product.description}</p>
-//            <h4 className={styles.cardPrice}>${product.price}</h4>
-//          </div>
-//          {/* <button onClick={handleButtonClick}><Button /></button>
-//          <button onClick={handleButtonClick}><Buttoninfo/></button> */}
-//           <button onClick={() => handleButtonClick('cart')}><Button /></button> {/* Botón para el carrito */}
-//           <button onClick={() => handleButtonClick('info')}><Buttoninfo/></button> {/* Botón para la información */}
-//        </div>
-//        {/* {showModal && <Modal message="El producto se agregó al carrito!" onClose={handleCloseModal} />}
-//         */}
-//         {showModal && modalType === 'cart' && <Modal message="El producto se agregó al carrito!" onClose={handleCloseModal} />} {/* Modal para el carrito */}
-//        {showModal && modalType === 'info' && <ProductsModal product={product} onClose={handleCloseModal} />} {/* Modal para la información del producto */}
-      
-//      </>
-//    );
-//  };
-
-//  export default ProductCard;
-// NO FUNCIONA 
-
-// import React, { useState } from 'react';
-// import Button from "../atoms/Button/Button";
-// import Modal from './CartCardModal';
-// import styles from "./Cards.module.css";
-// import Buttoninfo from '../atoms/Button/buttoninfo';
-// import ProductsModal from './ProductsModal';
-
-// const ProductCard = ({ product }) => {
-//   const [showModal, setShowModal] = useState(false);
-//   const [modalType, setModalType] = useState(null);
-
-//   const handleButtonClick = (type) => {
-//     setModalType(type); // Establecer el tipo de modal según el botón clicado
-//     setShowModal(true); // Mostrar el modal
-//   };
-
-//   const handleCloseModal = () => {
-//     setShowModal(false); // Cerrar el modal
-//     setModalType(null); // Reiniciar el tipo de modal
-//   };
-
-//   return (
-//     <>  
-//       <div className={styles.card}>
-//         <img src={product.imageUrl} alt={product.name} className={styles.img} />
-//         <div>
-//           <h5 className={styles.cardTitle}>{product.name}</h5>
-//           <p className={styles.cardText}>{product.description}</p>
-//           <h4 className={styles.cardPrice}>${product.price}</h4>
-//         </div>
-//         <button onClick={() => handleButtonClick('cart')}><Button /></button> {/* Botón para el carrito */}
-//         <button onClick={() => handleButtonClick('info')}><Buttoninfo /></button> {/* Botón para la información del producto */}
-//       </div>
-//       {showModal && modalType === 'cart' && <Modal message="¡El producto se agregó al carrito!" onClose={handleCloseModal} />} {/* Modal para el carrito */}
-//       {showModal && modalType === 'info' && <ProductsModal product={product} onClose={handleCloseModal} />} {/* Modal para la información del producto */}
-//     </>
-//   );
-// };
-
-// export default ProductCard;
-
-//FUNCIONAAAAAAAAAAA pero al seleccionar el boton no hace nada 
-
-  import React, { useState } from 'react';
-  import Button from "../atoms/Button/Button";
-  import CartCardModal from './CartCardModal';
-  import styles from "./Cards.module.css";
-  import Buttoninfo from '../atoms/Button/buttoninfo';
-  import ProductsModal from './ProductsModal';
-
-  const ProductCard = ({ product }) => {
-    const [showModal, setShowModal] = useState(false);
-    const [modalType, setModalType] = useState(null);
-
-    const handleButtonClick = (type) => {
-      setModalType(type); // Establecer el tipo de modal según el botón clicado
-      setShowModal(true); // Mostrar el modal
-    };
-
-    const handleCloseModal = () => {
-      setShowModal(false); // Cerrar el modal
-      setModalType(null); // Reiniciar el tipo de modal
-    };
-
-    return (
-      <>  
-        <div className={styles.card}>
-          <img src={product.imageUrl} alt={product.name} className={styles.img} />
-          <div>
-            <h5 className={styles.cardTitle}>{product.name}</h5>
-            <p className={styles.cardText}>{product.description}</p>
-            <h4 className={styles.cardPrice}>${product.price}</h4>
-          </div>
-          <button onClick={() => handleButtonClick('cart')}><Button /></button> {/* Botón para el carrito */}
-          <button onClick={() => handleButtonClick('info')}><Buttoninfo /></button> {/* Botón para la información */}
-          <button onClick={() => handleButtonClick('more')} className={styles.moreButton}>Ver más</button> {/* Botón "Ver más" */}
-        </div>
-        {showModal && modalType === 'cart' && <CartCardModal message="¡El producto se agregó al carrito!" onClose={handleCloseModal} isOpen={showModal} />} {/* Modal para el carrito */}
-        {showModal && modalType === 'info' && <ProductsModal product={product} onClose={handleCloseModal} isOpen={showModal} />} {/* Modal para la información */}
-        {showModal && modalType === 'more' && <ProductsModal product={product} onClose={handleCloseModal} isOpen={showModal} />} {/* Modal para "Ver más" */}
-      </>
-    );
+  const handleButtonClick = (type) => {
+    setModalType(type); // Establecer el tipo de modal según el botón clicado
+    setShowModal(true); // Mostrar el modal
   };
 
-  export default ProductCard;
+  const handleCloseModal = () => {
+    setShowModal(false); // Cerrar el modal
+    setModalType(null); // Reiniciar el tipo de modal
+  };
 
+  return (
+    <>
+      <div className={styles.card}>
+        <img src={product.imageUrl} alt={product.name} className={styles.img} />
+        <div>
+          <h5 className={styles.cardTitle}>{product.name}</h5>
+          <p className={styles.cardText}>{product.description}</p>
+          <h4 className={styles.cardPrice}>${product.price}</h4>
+        </div>
+        <button onClick={() => handleButtonClick('cart')}><Button /></button> {/* Botón para el carrito */}
+        <button onClick={() => handleButtonClick('info')}><Buttoninfo /></button> {/* Botón para la información */}
+        <button onClick={() => handleButtonClick('more')} className={styles.moreButton}>Ver más</button> {/* Botón "Ver más" */}
+      </div>
+      {showModal && modalType === 'cart' && <CartCardModal message="¡El producto se agregó al carrito!" onClose={handleCloseModal} isOpen={showModal} />} {/* Modal para el carrito */}
+      {showModal && modalType === 'info' && <ProductsModal product={product} onClose={handleCloseModal} isOpen={showModal} />} {/* Modal para la información */}
+      {showModal && modalType === 'more' && <ProductsModal product={product} onClose={handleCloseModal} isOpen={showModal} />} {/* Modal para "Ver más" */}
+    </>
+  );
+};
 
-//FUNCIONA PERO ERROR EN EL BOOTSTRAP
+export default ProductCard;
+
 
 
 
--- nelux-technology/src/molecules/ProductsModal.js
+++ nelux-technology/src/molecules/ProductsModal.js
@@ -1,95 +1,29 @@
-// import React, { useState } from 'react';
-// import Button from "../atoms/Button/Button";
-// import Modal from './CartCardModal';
-// import styles from "./Cards.module.css";
-// import ProductsMoldal from "./ProductsModal";
-// import Buttoninfo from '../atoms/Button/buttoninfo';
-// // import {Modal, ModalHeader,ModalBody, ModalFooter} from 'reactstrap';
-// import { ModalBody } from 'reactstrap';
 
-// const ProductCard = ({ product }) => {
-//   const [showModal, setShowModal] = useState(false);
+import React from 'react';
+import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
+import styles from "./Cards.module.css";
 
-//   const handleButtonClick = () => {
-//     setShowModal(true);
-//   };
+const ProductsModal = ({ product, onClose, isOpen }) => {
+  console.log("modal de producto", product, onClose, isOpen)
+  return (
+    <Modal isOpen={isOpen} toggle={onClose}>
+      <ModalHeader toggle={onClose}>Información del Producto</ModalHeader>
+      <ModalBody>
+        <img src={product.imageUrl} alt={product.name} className={styles.img} />
+        <div>
+          <h5 className={styles.cardTitle}>{product.name}</h5>
+          <p className={styles.cardText}>{product.description}</p>
+          <h4 className={styles.cardPrice}>${product.price}</h4>
+        </div>
+      </ModalBody>
+      <ModalFooter>
+        <Button color="secondary" onClick={onClose}>Cerrar</Button>
+      </ModalFooter>
+    </Modal>
+  );
+};
 
-//   const handleCloseModal = () => {
-//     setShowModal(false);
-//   };
-
-
-
-
-//   return (
-//     <>  
-//       <div className={styles.card}>
-//         <img src={product.imageUrl} alt={product.name} className={styles.img}/>
-//         <div>
-//           <h5 className={styles.cardTitle}>{product.name}</h5>
-//           <p className={styles.cardText}>{product.description}</p>
-//           <h4 className={styles.cardPrice}>${product.price}</h4>
-//         </div>
-//         <button onClick={handleButtonClick}><Button /></button>
-//       </div>
-//       {showModal && <Modal message="Ver mas" onClose={handleCloseModal} />}
-      
-//     </>
-//   );
-// };
-
-// export default ProductCard;
-// import React from 'react';
-// import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
-
-// const ProductsModal = ({ product, onClose }) => {
-//   return (
-//     <Modal isOpen={true} toggle={onClose}>
-//       <ModalHeader toggle={onClose}>Información del Producto</ModalHeader>
-//       <ModalBody>
-//         <img src={product.imageUrl} alt={product.name} style={{ width: '100%' }} />
-//         <h5>{product.name}</h5>
-//         <p>{product.description}</p>
-//         <h4>${product.price}</h4>
-//       </ModalBody>
-//       <ModalFooter>
-//         <Button color="secondary" onClick={onClose}>Cerrar</Button>
-//       </ModalFooter>
-//     </Modal>
-//   );
-// };
-
-// export default ProductsModal;
-
-
-
-
-
- import React from 'react';
- import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
- import styles from "./Cards.module.css";
-
- const ProductsModal = ({ product, onClose, isOpen }) => {
-  console.log("modal de producto",product,onClose,isOpen)
-   return (
-     <Modal isOpen={isOpen} toggle={onClose}>
-       <ModalHeader toggle={onClose}>Información del Producto</ModalHeader>
-       <ModalBody>
-       <img src={product.imageUrl} alt={product.name} className={styles.img}/>
-         <div>
-           <h5 className={styles.cardTitle}>{product.name}</h5>
-           <p className={styles.cardText}>{product.description}</p>
-           <h4 className={styles.cardPrice}>${product.price}</h4>
-         </div>
-       </ModalBody>
-       <ModalFooter>
-         <Button color="secondary" onClick={onClose}>Cerrar</Button>
-       </ModalFooter>
-     </Modal>
-   );
- };
-
- export default ProductsModal;
+export default ProductsModal;
 
 //FUNCIONA PERO TIRA ERROR DE BOOTSTRAP
 
