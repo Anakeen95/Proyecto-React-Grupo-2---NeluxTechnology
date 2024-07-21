5bd80f0a80de9917c89e6b7cd8cea136a2855293
root <root@anakeendev.com>
2024-07-20 21:59:45 -0300

    Added real time button disabling.

--- nelux-technology/src/atoms/Button/Button.js
+++ nelux-technology/src/atoms/Button/Button.js
@@ -1,41 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Button = ({ quantity, addToCart }) => {
+  // Maintains local state for quantity
+  const [localQuantity, setLocalQuantity] = useState(quantity); 
+
   const handleClick = () => {
-    if (quantity > 0) {
+    if (localQuantity > 0) {
       addToCart();
+      // Update local state after successful add to cart when server updates quantity)
+      setLocalQuantity(localQuantity - 1);
     }
   };
 
   return (
-    <button className="button" onClick={handleClick} disabled={quantity <= 0}>
-      {quantity > 0 ? 'Agregar al carrito' : 'Sin Stock'}
+    // Disables the button based on localQuantity (a.k.a quantity on Products from json-server)
+    <button className="button" onClick={handleClick} disabled={localQuantity <= 0}> 
+      {localQuantity > 0 ? 'Agregar al carrito' : 'Sin Stock'}
 
       <style jsx>{`
         .button {
           color: #00FDFB;
-          background-color: ${quantity > 0 ? '#1D1D1B' : '#6c757d'};
+          background-color: ${localQuantity > 0 ? '#1D1D1B' : '#6c757d'};
           padding: 6px 8px;
           border: none;
-          border-radius: .5rem;
-          font-size: .9rem;
-          cursor: ${quantity > 0 ? 'pointer' : 'not-allowed'};
+          border-radius: 0.5rem;
+          font-size: 0.9rem;
+          cursor: ${localQuantity > 0 ? 'pointer' : 'not-allowed'};
           text-transform: uppercase;
-          box-shadow: 0px 1px 1px 0.5px ${quantity > 0 ? '#00FDFB' : 'gray'};
+          box-shadow: 0px 1px 1px 0.5px ${
+            localQuantity > 0 ? '#00FDFB' : 'gray'
+          };
           padding-top: 5px;
         }
 
         .button:hover {
           font-weight: bold;
-          box-shadow: ${quantity > 0 ? '0px 1px 1px 1px #00FDFB' : 'none'};
-          opacity: ${quantity > 0 ? '0.70' : '1'};
-          -moz-opacity: ${quantity > 0 ? '.70' : '1'};
-          filter: ${quantity > 0 ? 'alpha(opacity=20)' : 'none'};
+          box-shadow: ${localQuantity > 0 ? '0px 1px 1px 1px #00FDFB' : 'none'};
+          opacity: ${localQuantity > 0 ? '0.70' : '1'};
+          -moz-opacity: ${localQuantity > 0 ? '.70' : '1'};
+          filter: ${localQuantity > 0 ? 'alpha(opacity=20)' : 'none'};
         }
 
         .button:active {
-          background-color: ${quantity > 0 ? '#4d4b55' : '#6c757d'};
-          transform: ${quantity > 0 ? 'scale(0.95)' : 'none'};
+          background-color: ${localQuantity > 0 ? '#4d4b55' : '#6c757d'};
+          transform: ${localQuantity > 0 ? 'scale(0.95)' : 'none'};
         }
       `}</style>
     </button>
