717b0f6f4baefca9b2670d3ec0b274f45d7685b2
root <root@anakeendev.com>
2024-07-21 14:46:32 -0300

    Navbar follow up to page when scrolling.

--- nelux-technology/src/organisms/Nav/Nav.js
+++ nelux-technology/src/organisms/Nav/Nav.js
@@ -1,19 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Nav.module.css';
 
 const Nav = ({ cartCount }) => {
     const [isOpen, setIsOpen] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
     };
 
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <div className={styles.nav}>
+        <div className={`${styles.nav} ${isScrolled ? styles.navScrolled : ''}`}>
             <div className={styles.logo}>
                 <img src="./images/NeluxTech-Isotipo-Negro.png" alt="Logo" />
             </div>
-            <div className={`${styles.navBar} ${isOpen ? styles.navBarOpen : ''}`}>
+            <ul className={`${styles.navBar} ${isOpen ? styles.navBarOpen : ''}`}>
                 <li className={styles.navBarItem}>
                     <a className={styles.navBarLink} href="#Home">Home</a>
                 </li>
@@ -26,10 +43,10 @@ const Nav = ({ cartCount }) => {
                 <li className={styles.navBarItem}>
                     <a className={styles.navBarLink} href="#Contacto">Contact</a>
                 </li>
-            </div>
+            </ul>
             <div className={styles.cartLogo}>
                 <img src="./images/Cart-Icon.png" alt="Cart-Logo" />
-                { cartCount>0 && <span className={styles.cartCount}>{cartCount}</span>}
+                { cartCount > 0 && <span className={styles.cartCount}>{cartCount}</span>}
             </div>
             <div className={styles.burgerMenu} onClick={toggleMenu}>
                 <div className={styles.burgerLine}></div>
