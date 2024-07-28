9e42e034dae3255ebcd35c59c87c02fa0fd7b0b2
root <root@anakeendev.com>
2024-07-28 18:51:00 -0300

    Added hover to the ´Vaciar Carrito´ button.

--- nelux-technology/src/molecules/Cart.js
+++ nelux-technology/src/molecules/Cart.js
@@ -7,29 +7,22 @@ const Cart = ({ cartItems, removeFromCart, clearCart, closeCart, isCartOpen }) =
     <div className={`${styles.cartContainer} ${isCartOpen ? styles.show : ''}`}>
       <div className={styles.cartContent}>
         <button className={styles.closeButton} onClick={closeCart}>
-          <svg
-            className={styles.closeIcon}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <line x1="18" y1="6" x2="6" y2="18" />
-            <line x1="6" y1="6" x2="18" y2="18" />
+          <svg className={styles.closeIcon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <line x1="18" y1="6" x2="6" y2="18"/>
+            <line x1="6" y1="6" x2="18" y2="18"/>
           </svg>
         </button>
         <div className={styles.cartHeader}>
           <h3>Carrito de Compras</h3>
         </div>
         {cartItems.length === 0 ? (
-          <p>No hay productos en el carrito.</p>
+          <p>
+            No hay productos en el carrito.
+          </p>
         ) : (
           <ul className={styles.cartItems}>
             {cartItems.map(item => (
-              <CartItem key={item.id} item={item} removeFromCart={removeFromCart} />
+              <CartItem key={item.id} item={item} removeFromCart={removeFromCart}/>
             ))}
           </ul>
         )}
@@ -42,6 +35,3 @@ const Cart = ({ cartItems, removeFromCart, clearCart, closeCart, isCartOpen }) =
 };
 
 export default Cart;
-
-
-
