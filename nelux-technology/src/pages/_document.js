d80eb644426960b48d26dd94f2188e7517b7a3e9
root <root@anakeendev.com>
2024-07-28 17:11:09 -0300

    Added Quicksand font and removed border on SectionCards Title.

--- nelux-technology/src/pages/_document.js
+++ nelux-technology/src/pages/_document.js
@@ -3,10 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head/>
+      <Head>
+        {/* Adds Quicksand Font to the whole site */}
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap"/>
+      </Head>
       <body>
-        <Main />
-        <NextScript />
+        <Main/>
+        <NextScript/>
       </body>
     </Html>
   );
