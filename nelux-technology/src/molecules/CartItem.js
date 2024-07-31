3fd41d27ba4ed91d428e19f3564f9cd96c79ae20
root <root@anakeendev.com>
2024-07-30 21:45:50 -0300

    Added modal when paying.

--- nelux-technology/src/molecules/CartItem.js
+++ nelux-technology/src/molecules/CartItem.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './CartItem.module.css';
+import Modal2 from './CartItemBuyModal.js';
 
 const CartItem = ({ item, incrementQuantity, decrementQuantity, removeFromCart }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => setModalOpen(false);
+  const confirmPayment = () => {
+    // Lógica para manejar el pago
+    alert('Pago confirmado');
+    closeModal();
+  };
+
   return (
     <li className={styles.cartItem}>
       <img src={item.imageUrl} alt={item.name} className={styles.cartItemImg}/>
@@ -26,9 +37,10 @@ const CartItem = ({ item, incrementQuantity, decrementQuantity, removeFromCart }
         <button className={styles.quantityButton} onClick={() => decrementQuantity(item.id)} disabled={item.quantity < 1}>
           -
         </button>
-        <button className={styles.payButton} onClick={() => alert('Pagar')}>
+        <button className={styles.payButton} onClick={openModal}>
           Pagar
         </button>
+        <Modal2 isOpen={isModalOpen} onClose={closeModal} onConfirm={confirmPayment}/>
       </div>
     </li>
   );
